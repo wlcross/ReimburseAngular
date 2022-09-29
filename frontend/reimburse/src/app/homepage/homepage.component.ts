842912acea9f375d6365f9b7f8b8768dc1e3c917
wlcross <william589@revature.net>
2022-09-28 21:49:26 -0400

    Variable Routing for managers

--- frontend/reimburse/src/app/homepage/homepage.component.ts
+++ frontend/reimburse/src/app/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  user = "";
+
+  constructor(private http:HttpClient, private router:Router) { }
 
   ngOnInit(): void {
+    let url = "http://localhost:8087/user/";
+    let uidsub = (this.http.get(url + sessionStorage.getItem("UID"))).subscribe (
+    e => {
+        console.log(e);
+
+        this.user = JSON.stringify(e);
+
+        let userObject = JSON.parse(this.user);
+
+        if (userObject.manager == true) {
+          this.router.navigate(['managerhomepage']);
+        }
+        
+        //this.router.navigate(['homepage'])
+    }
+    );
   }
 
 }
