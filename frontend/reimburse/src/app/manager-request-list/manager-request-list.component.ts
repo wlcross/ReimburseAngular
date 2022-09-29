11df7b7b365999fa13d12b834ac773fbcb97e25b
wlcross <william589@revature.net>
2022-09-29 10:40:51 -0400

    Manager Request List

--- frontend/reimburse/src/app/manager-request-list/manager-request-list.component.ts
+++ frontend/reimburse/src/app/manager-request-list/manager-request-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-manager-request-list',
@@ -7,9 +9,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagerRequestListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient, private router:Router) { }
 
   ngOnInit(): void {
+    let url = "http://localhost:8087/requests/open"
+    let uidsub = (this.http.get(url)).subscribe (
+      e => {
+          console.log(e);
+  
+          let requestObject = JSON.parse(JSON.stringify(e));
+  
+          let count = requestObject.length;
+          
+          //this.router.navigate(['homepage'])
+  
+          var table = document.createElement('table');
+                table.classList.add('table-bordered');
+                table.classList.add('tablespace');
+                let headers = table.insertRow();
+                {
+                    let link = headers.insertCell();
+                    link.appendChild(document.createTextNode("Link"));
+                    let emp = headers.insertCell();
+                    emp.appendChild(document.createTextNode("Employee"));
+                    let type = headers.insertCell();
+                    type.appendChild(document.createTextNode("Type"));
+                    let amount = headers.insertCell();
+                    amount.appendChild(document.createTextNode("Amount Requested"));
+                }
+
+                for( let i = 0; i < count; i ++) {
+                  let row = table.insertRow();
+
+                  let button = document.createElement('button');
+                  button.type = 'button';
+                  button.innerHTML = "View";
+
+                  button.onclick = function() {
+                      sessionStorage.setItem("request", requestObject[i].id);
+                      window.location.assign("requestViewManager.html");
+                  }
+
+                  let desc = row.insertCell();
+                  desc.appendChild(button);
+                  desc.classList.add("table-element");
+
+                  let emp = row.insertCell();
+                  emp.appendChild(document.createTextNode(requestObject[i].uID));
+                  emp.classList.add("table-element");
+
+                  let type = row.insertCell();
+                  type.appendChild(document.createTextNode(requestObject[i].category));
+                  type.classList.add("table-element");
+
+                  let amount = row.insertCell();
+                  amount.appendChild(document.createTextNode(("$" + parseInt(requestObject[i].owedAmount) / 100)));
+                  amount.classList.add("table-element");
+                }
+              let tableSpace = document.getElementById("tableSpace");
+              if (tableSpace != null) {
+                tableSpace.appendChild(table);
+              }
+              
+      }
+      );
   }
 
+  logout() {
+    this.router.navigate(['login']);
+  }
+
+  homepage() {
+    this.router.navigate(['homepage']);
+  }
 }
